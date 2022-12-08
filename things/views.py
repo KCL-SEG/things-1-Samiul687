b627bcf76db33d67df7d1b823741188a978c5cdb
Samiul I <99480280+Samiul687@users.noreply.github.com>
2022-12-08 08:37:23 +0000

    Bug fix

--- things/views.py
+++ things/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
+def thing(request):
+    return render(request, 'thing.html')
 
 # Create your views here.
-def home(request):
-    return render(request, 'home.html')
